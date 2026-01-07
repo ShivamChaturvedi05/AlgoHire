d469ff930a51bb1577a6753f4de92b7581335d80
Shivam Chaturvedi <ssshivam2005@gmail.com>
2026-01-07 19:48:55 +0530

    logout feature

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -1,11 +1,15 @@
 import { Router } from "express";
 import { registerUser,
         loginUser,
+        logoutUser
     } from "../controllers/user.controller.js";
-
+import { verifyJWT } from "../middlewares/auth.middleware.js";
 const router = Router();
 
 router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
+
+//Secure:
+router.route("/logout").post(verifyJWT, logoutUser);
 
 export default router;
