ca3e32f706c0dde5f12ce9cde2deb4a71542f21d
Shivam Chaturvedi <ssshivam2005@gmail.com>
2026-01-11 12:49:16 +0530

    token req improved

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -8,4 +8,34 @@ const api = axios.create({
   },
 });
 
+
+api.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    const originalRequest = error.config;
+
+    if (error.response?.status === 401 && !originalRequest._retry) {
+      originalRequest._retry = true;
+
+      try {
+        await axios.post(
+          'http://localhost:8000/api/v1/users/refresh-token', 
+          {}, 
+          { withCredentials: true }
+        );
+        return api(originalRequest);
+
+      } catch (refreshError) {
+        console.error("Session expired. Please login again.");
+        window.location.href = '/login';
+        return Promise.reject(refreshError);
+      }
+    }
+
+    return Promise.reject(error);
+  }
+);
+
 export default api;
