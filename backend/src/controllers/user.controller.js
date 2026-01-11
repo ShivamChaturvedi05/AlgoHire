aa1c6d7cce2a3f8990c7ab5c800ad6d6bf23d8f2
Shivam Chaturvedi <ssshivam2005@gmail.com>
2026-01-11 12:32:19 +0530

    ref-token secure

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -172,7 +172,7 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
         .json(
             new ApiResponse(
                 200, 
-                { accessToken, refreshToken: newRefreshToken }, 
+                {}, 
                 "Access token refreshed"
             )
         )
