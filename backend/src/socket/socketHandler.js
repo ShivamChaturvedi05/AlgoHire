640b1b719efa95e7ab007ae236e3585305c8451a
Shivam Chaturvedi <ssshivam2005@gmail.com>
2026-01-11 15:30:10 +0530

    improved socketHandler

--- backend/src/socket/socketHandler.js
+++ backend/src/socket/socketHandler.js
@@ -1,29 +1,59 @@
 import { Interview } from "../models/interview.model.js";
 
+const interviewers = {}; 
+
 const setupSocket = (io) => {
     io.on("connection", (socket) => {
         console.log("User connected:", socket.id);
 
-        socket.on("join-room", async ({ roomId, userId, role }) => {
+        socket.on("join-room", ({ roomId, userId, role }) => {
             socket.join(roomId);
-            
-            if (role === 'interviewer') {
+
+            if (role === "interviewer") {
+                interviewers[roomId] = socket.id;
+                
                 socket.emit("room-joined", { status: "approved" });
             } else {
-                socket.to(roomId).emit("user-waiting", { 
-                    userId, 
-                    socketId: socket.id 
-                });
+                const interviewerSocketId = interviewers[roomId];
+
+                if (interviewerSocketId) {
+                    io.to(interviewerSocketId).emit("user-waiting", {
+                        userId,
+                        socketId: socket.id
+                    });
+                } else {
+                    console.log("No interviewer found for room:", roomId);
+                }
             }
         });
 
         socket.on("admit-candidate", ({ socketId }) => {
-    
             io.to(socketId).emit("room-joined", { status: "approved" });
-
         });
 
-        
+        socket.on("code-change", ({ roomId, code }) => {
+            socket.to(roomId).emit("code-update", code);
+        });
+
+        socket.on("language-change", ({ roomId, language }) => {
+            io.to(roomId).emit("language-update", language);
+        });
+
+        socket.on("whiteboard-draw", ({ roomId, data }) => {
+            socket.to(roomId).emit("whiteboard-update", data);
+        });
+
+        socket.on("disconnect", () => {
+            console.log("User disconnected:", socket.id);
+
+            for (const roomId in interviewers) {
+                if (interviewers[roomId] === socket.id) {
+                    delete interviewers[roomId];
+                    console.log(`Interviewer left room: ${roomId}`);
+                    break;
+                }
+            }
+        });
     });
 };
 
