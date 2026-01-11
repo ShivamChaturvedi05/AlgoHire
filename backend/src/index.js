ff849b2ea5010cc624f7aa343c5cb7de7ebc8a20
Shivam Chaturvedi <ssshivam2005@gmail.com>
2026-01-11 16:05:00 +0530

    Socket Config

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,24 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 import { app } from "./app.js";
+import { Server } from "socket.io";
+import http from "http";
+import setupSocket from "./socket/socketHandler.js";
 
 dotenv.config({
     path: './.env'
 });
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"]
+    }
+});
+
+setupSocket(io);
 
 connectDB()
 .then(() => {
