4c7735f2d2ae28efe529d8a7c68154b1586bcd3c
Shivam Chaturvedi <ssshivam2005@gmail.com>
2026-08-30 22:37:32 +0530

    Global error handler added

--- backend/src/app.js
+++ backend/src/app.js
@@ -9,9 +9,9 @@ app.use(cors({
     credentials: true
 }));
 
-app.use(express.json({limit: "16kb"}));
+app.use(express.json({ limit: "16kb" }));
 
-app.use(express.urlencoded({extended: true, limit: "16kb"}));
+app.use(express.urlencoded({ extended: true, limit: "16kb" }));
 
 app.use(express.static("public"));
 
@@ -29,5 +29,17 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/rooms", roomRouter);
 app.use("/api/v1/compiler", compilerRouter);
 app.use("/api/v1/questions", questionRouter);
+
+//GLobal error handler:
+app.use((err, req, res, next) => {
+    const statusCode = err.statusCode || 500;
+    const message = err.message || "Something went wrong";
+
+    return res.status(statusCode).json({
+        success: false,
+        message: message,
+        errors: err.errors || []
+    });
+});
 
 export { app };
